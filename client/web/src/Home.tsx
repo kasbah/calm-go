65ccf00252ce9223d844b243360c58c2ba560bc6
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2022-02-26 17:08:24 +0000

    Work on style for Home

--- client/web/src/Home.tsx
+++ client/web/src/Home.tsx
@@ -3,9 +3,16 @@ import { HathoraClient, UpdateArgs } from "../.hathora/client";
 
 function Home() {
   return (
-    <div>
+    <div className="flex h-screen flex-col ">
       <div className="flex justify-center">
-        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="font-medium leading-tight text-5xl mt-6 mb-2 text-gray-800">
+          Calm Go
+        </h1>
+      </div>
+      <div className="flex h-48 place-items-center justify-center">
+        <button className="bg-transparent hover:border-gray-300 hover:text-gray-500 font-semibold py-2 px-4 border border-gray-700 text-gray-700">
+          Create a Game
+        </button>
       </div>
     </div>
   );
--- client/web/tailwind.config.cjs
+++ client/web/tailwind.config.cjs
@@ -5,7 +5,33 @@ module.exports = {
   content: ["./index.html", "src/**/*.tsx"].map((str) =>
     path.relative(process.cwd(), path.resolve(__dirname, str))
   ),
-  theme: {},
-  variants: {},
+  theme: {
+    colors: {
+      transparent: "transparent",
+      gray: colors.gray,
+      red: colors.rose,
+      green: colors.teal,
+      blue: colors.cyan,
+      orange: colors.amber,
+      white: colors.white,
+      black: colors.black,
+    },
+    fontFamily: {
+      sans: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Oxygen",
+        "Ubuntu",
+        "Cantarell",
+        "Fira Sans",
+        "Droid Sans",
+        "Helvetica Neue",
+        "sans-serif",
+      ],
+    },
+    extend: {},
+  },
   plugins: [require("@tailwindcss/forms")],
 };
